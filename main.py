38bf5277ce61b7e68f3881216f32731755e90309
qwertyuiop7161 <113679020+qwertyuiop7161@users.noreply.github.com>
2022-09-18 14:10:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,6 @@ bot = Client("bot",
 auth_users = [
     int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
 sudo_users = auth_users
-sudo_groups = [
-    int(chat) for chat in os.environ.get("GROUPS").split(",") if chat != '']
 
 keyboard = InlineKeyboardMarkup(
     [
